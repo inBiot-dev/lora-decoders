8a13fe627fc6b7e0ddd21fe6aef08be9755765c4
Miguel Ferrer Ferrández <115996960+MiguelFerrerF@users.noreply.github.com>
2024-05-27 10:42:21 +0200

    Added support for Chirpstack V3 & V4

--- V1.1/payload.js
+++ V1.1/payload.js
@@ -1,14 +1,34 @@
-function Decoder(buffer) {
+// Chirpstack v4
+function decodeUplink(input) {
+  var decoded = InbiotDeviceDecode(input.bytes);
+  return { data: decoded };
+}
+
+// Chirpstack v3
+function Decode(fPort, bytes) {
+  return InbiotDeviceDecode(bytes);
+}
+
+// The Things Network
+function Decoder(bytes, port) {
+  return InbiotDeviceDecode(bytes);
+}
+
+function InbiotDeviceDecode(bytes) {
   const decoded = {};
 
-  switch (buffer[0]) {
+  switch (bytes[0]) {
     case 0:
-      decoded.timeToSend = buffer[1];
-      decoded.ventilation = buffer[2];
-      decoded.ledStatus = buffer[3] ? "true" : "false";
+      // TIME TO SEND
+      decoded.timeToSend = bytes[1];
+      // VENTILATION TYPE
+      decoded.ventilation = bytes[2];
+      // LED CONFIGURATION
+      decoded.ledStatus = bytes[3] ? "true" : "false";
       break;
     case 1:
-      decoded.type = customTextDecoder(buffer, 27, 31);
+      // MICA TYPE
+      decoded.type = customTextDecoder(bytes, 27, 31);
       if (decoded.type === "\u0000\u0000\u0000\u0000") {
         decoded.type = "NULL";
       }
@@ -20,82 +40,106 @@ function Decoder(buffer) {
         NULL: true,
       };
       if (typeProperties[decoded.type]) {
-        decoded.temperature = getUint16(buffer, 1, 2) / 10.0;
-        decoded.humidity = getUint16(buffer, 3, 4) / 10.0;
-        decoded.co2 = getUint16(buffer, 5, 6);
+        // TEMPERATURE
+        decoded.temperature = getUint16(bytes, 1, 2) / 10.0;
+        // HUMIDITY
+        decoded.humidity = getUint16(bytes, 3, 4) / 10.0;
+        // CO2
+        decoded.co2 = getUint16(bytes, 5, 6);
 
         if (decoded.type !== "MINI") {
-          decoded.tvoc = getUint16(buffer, 9, 10);
-          decoded.pm2_5 = getUint16(buffer, 13, 14);
-          decoded.pm10 = getUint16(buffer, 17, 18);
+          // TVOC
+          decoded.tvoc = getUint16(bytes, 9, 10);
+          // PM2.5
+          decoded.pm2_5 = getUint16(bytes, 13, 14);
+          // PM10
+          decoded.pm10 = getUint16(bytes, 17, 18);
         }
         if (
           decoded.type === "PLUS" ||
           decoded.type === "WELL" ||
           decoded.type === "NULL"
         ) {
-          decoded.ch2o = getUint16(buffer, 7, 8);
-          decoded.pm1_0 = getUint16(buffer, 11, 12);
-          decoded.pm4 = getUint16(buffer, 15, 16);
+          // CH2O
+          decoded.ch2o = getUint16(bytes, 7, 8);
+          // PM1.0
+          decoded.pm1_0 = getUint16(bytes, 11, 12);
+          // PM4
+          decoded.pm4 = getUint16(bytes, 15, 16);
         }
         if (decoded.type === "WELL" || decoded.type === "NULL") {
-          decoded.o3 = getUint16(buffer, 19, 20);
+          // O3
+          decoded.o3 = getUint16(bytes, 19, 20);
           if (decoded.o3 === 0xffff) {
             decoded.o3 = "Preheating";
           }
-          decoded.no2 = getUint16(buffer, 21, 22);
+          // NO2
+          decoded.no2 = getUint16(bytes, 21, 22);
           if (decoded.no2 === 0xffff) {
             decoded.no2 = "Preheating";
           }
-          decoded.co = getUint16(buffer, 23, 24);
+          // CO
+          decoded.co = getUint16(bytes, 23, 24);
           if (decoded.co !== 0xffff) {
-            decoded.co /=  10.0;
+            decoded.co /= 10.0;
           } else {
             decoded.co = "Preheating";
           }
         }
-        decoded.vIndex = buffer[32];
-        decoded.counter = getUint16(buffer, 25, 26);
+        // VENTILATION INDEX
+        decoded.vIndex = bytes[32];
+        // MESSAGE COUNTER
+        decoded.counter = getUint16(bytes, 25, 26);
       }
       break;
     case 2:
-      decoded.fwVersion = customTextDecoder(buffer, 1, 4);
-      decoded.model = customTextDecoder(buffer, 4, 21);
-      decoded.micaType = customTextDecoder(buffer, 21, 30);
-      decoded.mac = buffer
+      // FIRMWARE VERSION
+      decoded.fwVersion = customTextDecoder(bytes, 1, 4);
+      // MODEL
+      decoded.model = customTextDecoder(bytes, 4, 21);
+      // MICA TYPE
+      decoded.micaType = customTextDecoder(bytes, 21, 30);
+      // MAC ADDRESS
+      decoded.mac = bytes
         .slice(30, 36)
         .map((b) => b.toString(16).padStart(2, "0"))
         .join(":")
         .toUpperCase();
-      decoded.modbusAddress = buffer[36];
-      decoded.modbusParity = buffer[37];
-      decoded.modbusBaudRate = getUint32(buffer, 38);
+      // ----- ONLY MODBUS CONFIGURATION -----
+
+      // MODBUS ADDRESS
+      decoded.modbusAddress = bytes[36];
+      // MODBUS PARITY
+      decoded.modbusParity = bytes[37];
+      // MODBUS BAUD RATE
+      decoded.modbusBaudRate = getUint32(bytes, 38);
       break;
     default:
   }
   return decoded;
 }
 
-function customTextDecoder(buffer, start, end) {
+function customTextDecoder(bytes, start, end) {
   let result = "";
   for (let i = start; i < end; i++) {
-    result += String.fromCharCode(buffer[i]);
+    result += String.fromCharCode(bytes[i]);
   }
   return result;
 }
 
-function getUint16(buffer, first, second) {
+function getUint16(bytes, first, second) {
   let result = "";
-  result = (buffer[first] << 8) | buffer[second];
+  result = (bytes[first] << 8) | bytes[second];
   return result;
 }
 
-function getUint32(buffer, start) {
+function getUint32(bytes, start) {
   let result = 0;
   result =
-    (buffer[start] << 24) |
-    (buffer[start + 1] << 16) |
-    (buffer[start + 2] << 8) |
-    buffer[start + 3];
+    (bytes[start] << 24) |
+    (bytes[start + 1] << 16) |
+    (bytes[start + 2] << 8) |
+    bytes[start + 3];
   return result;
 }
+
